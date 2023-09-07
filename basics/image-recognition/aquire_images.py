a7cf3415ae74193b78e53c3c0fa59af803f6aa9c
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-09-07 09:45:26 +0200

    fixed: let camera settle, image display

--- basics/image-recognition/aquire_images.py
+++ basics/image-recognition/aquire_images.py
@@ -1,6 +1,5 @@
 import cv2
 import requests
-from time import sleep 
 import os
 import sys
 from datetime import datetime
@@ -27,18 +26,22 @@ def init():
     print(f"Saving images into directory {subdir}")
     os.mkdir(subdir)
 
+    for i in range(60):
+        ret, frame = cam.read()
+        cv2.imshow("Image", frame)
+        cv2.waitKey(10)  # Needed for the Display Window
 
 def getImage(led):
     url = f"http://192.168.4.1/cmd?led={led}"
     print(url)
     r = requests.get(url, timeout=3)
-    sleep(0.1)
-    
+    cv2.waitKey(100)  # Wait for the camera to settle
+
     ret, frame = cam.read()
+    cv2.imshow("Image", frame);
     filename = "%s/%04d.png" % (subdir, led)
     print(f"Saved image to {filename}")
     cv2.imwrite(filename, frame)
-    #sleep(0.05)
 
 
 init();
