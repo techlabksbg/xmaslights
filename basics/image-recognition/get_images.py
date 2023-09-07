f69d20076a8bc0e55f1b884d583e23fdcce33909
pizzamoeger <osshannahsophie@gmail.com>
2023-09-07 11:12:03 +0200

    getting images and finding pos in seperate files

--- basics/image-recognition/get_images.py
+++ basics/image-recognition/get_images.py
@@ -15,39 +15,15 @@ def init():
 
 	if not os.path.isdir(temp):
 		print(f"Creating directory {temp}")
-		os.mkdir("temp")
+		os.mkdir(temp)
 	
 	subdir=temp+"/"+datetime.now().strftime("%Y%m%d_%H%M%S")
+
 
 	print(f"Creating directory {subdir}")
 	os.mkdir(subdir)
 	
 	return cap, subdir
-	
-	
-def find_brightest_pixel(image):
-	# Convert the image to grayscale for simplicity
-	grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-	# Find the coordinates of the brightest pixel
-	(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(grayscale_image)
-	brightest_pixel = maxLoc
-	brightness = maxVal
-
-	return brightest_pixel, brightness
-
-def get_image(dir, led):
-	# Open the webcam
-	ret, frame = cap.read()
-
-	# save image to file into dir
-	filename = f"{dir}/{led:04d}.png"
-	# cv2.imshow(filename, frame)
-	cv2.imwrite(filename, frame)
-
-	# find brightest pixel and print it's coordinates and brightness
-	brightest_pixel_coords, brightness_value = find_brightest_pixel(frame)
-	print(str(led) + ": Location " + str(brightest_pixel_coords) + ", Value " + str(brightness_value))
 	
 def show_led(led):
 	# turning on led
@@ -57,12 +33,12 @@ def show_led(led):
 	if(r.status_code != 200):
 		print(f"Error: {r.status_code} when requesting {url}")
 
+
 cap, subdir = init()
 
 for led in range(LED_COUNT):
 	show_led(led)
 	sleep(0.5)
-	get_image(subdir, led)
 
 cap.release()
 cv2.destroyAllWindows()
