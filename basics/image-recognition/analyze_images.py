63521edfc594563acfd759a2a47af8de3892c466
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-09-14 09:09:15 +0200

    simplistic analyzer, added treehight in campos.txt

--- basics/image-recognition/analyze_images.py
+++ basics/image-recognition/analyze_images.py
@@ -3,6 +3,9 @@ import os
 import sys
 import re
 
+
+n=0 #define immage counter
+
 # Produces an array with all files
 def init():
     if len(sys.argv)<2:
@@ -13,12 +16,27 @@ def init():
     if not os.path.isdir(dir):
         print(f"Das Verzeichnis {dir} existiert nicht.")
         exit()
-    return sorted([dir+"/"+f for f in os.listdir(dir) if re.match(r"^\d{4}\.png",f)])
+    if os.path.isfile(dir+"/campos.txt"):
+        with open(dir+"/campos.txt") as f:
+            print(f.read(), end="")
+    else:
+        print("? ? ?\n?");
+    return sorted([dir+"/"+f for f in os.listdir(dir) if re.match(r"\d{4}\.png$",f)])
 
+def find_brightest_pixel(image):
+    # Convert the image to grayscale for simplicity
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Find the coordinates of the brightest pixel
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(grayscale_image)
+    brightest_pixel = maxLoc
+    brightness = maxVal
+
+    return brightest_pixel, brightness
 
 files = init()
-for f in files:
-    print(f"Loading {f}...")
+for n,f in enumerate(files):
     image = cv2.imread(f)
-    cv2.imshow("Image", image)
-    cv2.waitKey(500)  # Waits for 500ms and thus allows the image to be displayed
+    brightest_pixel_coords, brightness_value = find_brightest_pixel(image)
+    x,y = brightest_pixel_coords
+    print("%04d %d %d 0.5" % (n, x, y))
