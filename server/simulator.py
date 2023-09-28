2130c087e349a8848c653843ecf89a14426671aa
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-09-28 08:27:50 +0200

    simulator mit fehlender zentralprojektion

--- server/simulator.py
+++ server/simulator.py
@@ -1,34 +1,48 @@
 import pygame
 import numpy as np
 import math
+
+# Eigene Dinge importieren
 from python.leds import LEDs
 from python.rainbow3d import Rainbow3d
 
+aufloesung = (1280, 720)
 
+# Punkte einlesen
 def init():
+    # Koordinaten der LEDs einlesen
+    # Als np.array, geeignet für Matrix-Operationen, 
+    # x-Koordinate von Punkt 5 ist poins[0][5] (Erst Zeile, dann Spalte)
     with open("python/3ddata.txt", "r") as f:
         points = np.column_stack([[float(c) for c in l.split(" ")][0:3] for l in f.readlines()])
     return points
 
+# 3D-Rotationsmatrix um z
 def rotationz(w):
     c = math.cos(w)
     s = math.sin(w)
     return np.column_stack([[c,s,0],[-s,c,0],[0,0,1]])
 
-def transform(points):
+def getHeight(points):
     minz = min(points[2])
     maxz = max(points[2])
-    scale = 640/(maxz-minz)
-    return np.column_stack([[-scale,0,0,0],[0,scale,0,0],[0,0,-scale,0],[500, 0, 320, 1]])
+    return maxz-minz
 
+# TODO
+# Zentralprojektion auf die y,z-Ebene
+def projektion(x,y,z, scale, aufloesung):
 
-    
+    a = y+aufloesung[0]/2
+    b = -z + aufloesung[1]
+    r = 4  # Optional: Radius je nach Distanz für besseren 3D-Effekt.
+
+    return a,b,r
 
 def run(points, leds, prog):
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode(aufloesung)
     clock = pygame.time.Clock()
-    t = transform(points)
+    height = getHeight(points)
     running = True
     w = 0.0
     while running:
@@ -36,19 +50,22 @@ def run(points, leds, prog):
             if event.type == pygame.QUIT:
                 running = False
         screen.fill("black")
-        rotated = np.matmul(rotationz(w), points)
-        rotated = np.append(rotated, [np.ones(leds.n)], axis=0)
 
-        transformed = np.matmul(t, rotated)
-        
+        # Punkte rotieren
+        rotated = np.matmul(rotationz(w), points)
+
+        # Farben berechnen 
         prog.step(leds, points)
+
+        # Alle Leds zeichnen
         for l in range(leds.n):
+            # Farbe für LED l auslesen und konvertieren
             c = pygame.Color(int(leds.leds[l][0]), int(leds.leds[l][1]), int(leds.leds[l][2]))
-            s = 500/transformed[0][l]
-            pygame.draw.circle(screen,
-                                c,
-                               (transformed[1][l]*s+640, transformed[2][l]*s+360),
-                               4)
+
+            x,y,r = projektion(rotated[0][l], rotated[1][l], rotated[2][l], height, aufloesung)
+
+            # Kreis zeichnen
+            pygame.draw.circle(screen, c, (x, y), r)
 
         pygame.display.flip()
 
