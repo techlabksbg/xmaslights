c0541e44d8c0bed8be695e8454a55daa70d16bb1
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-09-28 09:58:32 +0200

    now using correcty spelled period parameter

--- server/python/rainbow3d.py
+++ server/python/rainbow3d.py
@@ -5,10 +5,10 @@ import colorsys
 class Rainbow3d(Programm):
     def __init__(self):
         self.start = time.time()
-        self.perdiod = 5
+        self.period = 5
 
     def step(self, leds, points):
-        dt = time.time()-self.start
+        dt = (time.time()-self.start)/self.period
         for l in range(leds.n):
             h = (points[2][l]/100+dt)%1
             c : list(int,int,int) = [int(x*255) for x in colorsys.hsv_to_rgb(h,1.0,1)]
