139835c7adf854a57c5f44ebb3add43051fdb07d
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-11-07 19:46:44 +0100

    major cleanup

--- server/python/main.py
+++ server/python/main.py
@@ -6,59 +6,79 @@ import numpy as np
 from config import Config
 from filewatcher import FileWatcher
 
-config = Config()
-config.registerKey('brightness', {'type':float, 'low':0.01, 'high':1.0, 'default':0.2})
-config.registerKey('saturation', {'type':float, 'low':0.0, 'high':1.0, 'default':1.0})
-config.registerKey('period', {'type':float, 'low':1, 'high':20, 'default':5})
-config.registerKey('color', {'type':'color', 'default':[255,255,255]})
+class XmaslightsServer():
+    def __init__(self, prgs):
+        self.leds = LEDs(800, (1,0,2))  # GRB color order
+        self.programNames = prgs
+        
+        self.initConfig()
+        self.startServers()
+        self.get3dData()
+        self.importProgams()
+    
+    def initConfig(self):
+        self.config = Config()
+        self.config.registerKey('brightness', {'type':float, 'low':0.01, 'high':1.0, 'default':0.2})
+        self.config.registerKey('saturation', {'type':float, 'low':0.0, 'high':1.0, 'default':1.0})
+        self.config.registerKey('period', {'type':float, 'low':1, 'high':20, 'default':5})
+        self.config.registerKey('color', {'type':'color', 'default':[255,255,255]})
+        self.config.registerKey('prg', {'type':str, 'default':self.programNames[0], 'allowed':self.programNames, 'minage':0.3})  # At lest 3 secs since last request to change this setting.
+
+    def startServers(self):
+        self.http = httpServer(self.config)  # Start and run http server
+        self.udp = UDP_Server()  # Init udp server (will run on the main thread)
+
+    def get3dData(self):
+        with open("3ddata.txt", "r") as f:
+            self.points = np.column_stack([[float(c) for c in l.split(" ")][0:3] for l in f.readlines()])
+
+    def importProgams(self):
+        modules = [__import__(m.lower()) for m in self.programNames]
+        self.programs = {self.programNames[i]:getattr(m, self.programNames[i])(self.config) for i,m in enumerate(modules)}
+
+    def step(self):
+        # Wait and receive UPD packets
+        stop = time.time()+0.016
+        while time.time()<stop:
+            time.sleep(0.001)
+            self.udp.loop()
+        # Compute new frame
+        self.programs[self.config['prg']].step(self.leds, self.points)
+        # Send frame if changes were made
+        if self.leds.changed:
+            self.clientPresent = self.udp.send(self.leds.bin())
+            if (self.clientPresent):
+                self.frames+=1
+            else:
+                self.frames = 0
+                self.start = time.time()
 
 
-http = httpServer(config)  # Start and run http server
-
-server = UDP_Server()  # Start udp server (will run on the main thread)
-
-with open("3ddata.txt", "r") as f:
-    points = np.column_stack([[float(c) for c in l.split(" ")][0:3] for l in f.readlines()])
-
-leds = LEDs(800, (1,0,2))  # GRB color order
-
-programNames = ["Example", "SingleLED", "Rainbow3d", "Kugeln"]
-config.registerKey('prg', {'type':str, 'default':'Example', 'allowed':programNames, 'minage':0.3})  # At lest 3 secs since last request to change this setting.
-modules = [__import__(m.lower()) for m in programNames]
-programs = {programNames[i]:getattr(m, programNames[i])(config) for i,m in enumerate(modules)}
-
-
-start = time.time()
-nextTime = start+5
-f = 0
-
-fileWatcher = FileWatcher()
-
-while http.thread.is_alive:  # Exit program if http server crashes
-    stop = time.time()+0.016
-    while time.time()<stop:
-        time.sleep(0.001)
-        server.loop()
-    programs[config['prg']].step(leds, points)
-
-    if leds.changed:
-        clientPresent = server.send(leds.bin())
-        if (clientPresent):
-            f+=1
-        else:
-            f = 0
-            start = time.time()
-
-    if time.time()>nextTime:
-        nextTime = time.time()+5
-        t = time.time()-start
+    def debug(self):
+        self.nextTime = time.time()+5
+        t = time.time()-self.start
         if t>0:
-            fps = f/t
-            print("[%s] %d frames in %.1f secs: %.1f fps, %s" % (config['prg'], f, t, fps, "connected" if clientPresent else "not connected"))
+            fps = self.frames/t
+            print("[%s] %d frames in %.1f secs: %.1f fps, %s" % (self.config['prg'], self.frames, t, fps, "connected" if self.clientPresent else "not connected"))
         else:
             print("No connection")
-        if fileWatcher.new_files():
-            print("New files detected, stopping server")
-            exit(0)
 
 
+    def run(self):
+        self.start = time.time()
+        self.nextTime = self.start+5
+        self.frames = 0
+
+        fileWatcher = FileWatcher()
+
+        while self.http.thread.is_alive:  # Exit program if http server crashes
+            self.step()
+        
+            if time.time()>self.nextTime:
+                self.debug()
+                if fileWatcher.new_files():
+                    print("New files detected, stopping server")
+                    exit(0)
+
+
+XmaslightsServer(["Example", "SingleLED", "Rainbow3d", "Kugeln"]).run()
