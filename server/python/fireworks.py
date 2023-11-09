cb1841136d0b91ba2b1089d928ff74049799ba31
pizzamoeger <osshannahsophie@gmail.com>
2023-11-09 11:18:09 +0100

    more efficient program

--- server/python/fireworks.py
+++ server/python/fireworks.py
@@ -1,17 +1,18 @@
 from program import Program
 import time
 import math
-import colorsys
+import random
 import numpy as np
 
 class Fireworks(Program):
     def __init__(self, config):
         self.config = config
         self.start = time.time()
-        self.r = 15
+        self.r = 10
         self.g = 9.81
-        self.c = 150
-        self.col = [255, 0, 0]
+        self.c_f = 250
+        self.c_p = 10
+        self.col = [random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)]
         self.particles = []
 
     def xyz(self, time, particle): # returns x,y,z coordinates of particle at time
@@ -21,9 +22,9 @@ class Fireworks(Program):
         return x,y,z
     
     def get_particles(self, sphere): # sets self.particles to list of evenly distributed particles around sphere
-        num_particles = 100
+        num_particles = 30
 
-        phi = math.pi * (math.sqrt(5.) - 1.)  # golden angle in radians
+        phi = random.uniform(0.0, 1.0) * math.pi * (math.sqrt(5.) - 1.)  # golden angle in radians
         points = []
 
         for i in range(num_particles):
@@ -42,44 +43,53 @@ class Fireworks(Program):
             y = sphere[1]+p[1]
             z = sphere[2]+p[2]
 
-            theta = math.atan2(y, x)
-            phi = math.atan2(math.sqrt(x**2+z**2), y)
+            theta = math.atan2(p[1], p[0])
+            phi = math.atan2(math.sqrt(p[0]**2+p[1]**2), p[2])
 
-            self.particles.append({'x0':x, 'y0':y, 'z0':z, 'theta':theta, 'phi':phi, 'v0':10, 'color':self.col})
+            self.particles.append({'x0':x, 'y0':y, 'z0':z, 'theta':theta, 'phi':phi, 'v0':20, 'color':self.col, 't0':time.time()})
         
 
     def step(self, leds, points):
 
         dt = (time.time()-self.start)/self.config['period']
-        z = min(points[2])+dt*self.c
+        z = min(points[2])+dt*self.c_f
         firework = [0, 0, z]
 
         if (z > max(points[2])-2*self.r):
             self.start = time.time()
+            dt = 0
             
-            self.particles = []
+            # explode firework
             self.get_particles(firework)
             
-            dt = 0
             z = min(points[2])
             firework = [0, 0, z]
 
-            self.col = [self.col[2]]+self.col[:2]
-            # explode firework
+            self.col = [random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)]
 
         
+        new_particles = [] # get xyz of all particles and remove particles that are out of range
+        for p in self.particles:
+            dt = (p['t0']-time.time())/self.config['period']
+            x,y,z = self.xyz(dt*self.c_p, p)
+            p['x'] = x
+            p['y'] = y
+            p['z'] = z
+            if (z > min(points[2]) and y < max(points[1]) and y > min(points[1]) and x < max(points[0]) and x > min(points[0])):
+                new_particles.append(p)
+        self.particles = new_particles
+
         for l in range(leds.n):
-            c = [100, 100, 100]
+            col = [50, 50, 50]
+            col = [0, 0, 0]
             
             v = np.linalg.norm(points[:,l]-firework) # if led is in range of firework
             if v < self.r:
-                c = self.col
+                col = self.col
             
             for p in self.particles:
-                x,y,z = self.xyz(dt*self.c/30, p)
-                v = np.linalg.norm(points[:,l]-[x,y,z])
-                if v < 10:
-                    c = p['color']
+                v = np.linalg.norm(points[:,l]-[p['x'], p['y'], p['z']])
+                if v < 8:
+                    col = p['color']
             
-            leds.setColor(l, c)
-        
+            leds.setColor(l, col)
