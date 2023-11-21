672d043e156408864dd0c492525416711ba34de4
Constantin <knights-pennies-0t@icloud.com>
2023-11-21 15:03:28 +0100

    Finished getting Pixel Values and asigning them to pixels

--- server/python/rickroll.py
+++ server/python/rickroll.py
@@ -1,39 +1,51 @@
 import cv2
+import numpy as np
 
-cap= cv2.VideoCapture('automatic.mp4')
+cap= cv2.VideoCapture('testvideo.mp4')
 success, image = cap.read()
-count = 0
+count = 1
 converted = []
 values = []
 ledValues = []
 ledCoordinates = [line.split(' ') for line in open("3ddata.txt")]
 ledCoordinates = [[float(i) for i in c]for c in ledCoordinates]
+ledCoordinates = [[i+55 for i in c]for c in ledCoordinates]
+ledCoordinates = [[round(i) for i in c]for c in ledCoordinates]
 print(ledCoordinates)
+f = f = open("ledData", "w")
+
 
 
 print("I am in success")
 while success:
   try:
     success,image = cap.read()
-    resize = cv2.resize(image, (40, 100), interpolation = cv2.INTER_LINEAR) 
+    resize = cv2.resize(image, (300, 300), interpolation = cv2.INTER_LINEAR) 
     converted.append(resize)
     cv2.imwrite("%03d.jpg", resize) # for debugging reasons
-    for x in range(40):
-      for y in range(100):
-        v = (resize[y, x])
-        if ledCoordinates[count] >= [x-5,y-5,0,0] and ledCoordinates[count] <= [x+5,y+5,100,2]:
-        #print("Pixel at ({}, {}) - Value {} ".format(x,y,v))
-          ledValues[count] = (v)
-          count += 1
+    c = 0
+    for i in ledCoordinates:
+        x = ledCoordinates[c]
+        x = x[1]
+        y = ledCoordinates[c]
+        y = y[2]
+        #print(x,y)
+        v = (resize[x, y])
+        #print("Pixel at (",x,",",y,") - Value ",v)
+        #v = str(v)
+        ledValues.append(v)
+        c += 1
 
-        
+
   except Exception as e:
-    print(str(e))
-count = -1
+    print("No value: ", str(e))
 print("Conversion finished")
+
+print(ledValues)
+f.write(str(ledValues))
 
 cap.release()
 cv2.destroyAllWindows()
 
-print(ledValues)
+
  
