2a39451e34bb49b4119d9584fc7011fc02561397
Constantin <knights-pennies-0t@icloud.com>
2023-11-23 11:10:02 +0100

    fixed merge conflict

--- server/python/animations_available/video.py
+++ server/python/animations_available/video.py
@@ -4,30 +4,26 @@ import colorsys
 import time
 import cv2
 
-<<<<<<< HEAD
+
 # Import von YouTube ist kaum möglich. Das muss direkt von einer Datei geschehen.
 # Vorschlag: Neues Verzeichnis 'video', das in .gitignore eingetragen wird.
 # Die Videos dort drin platzieren, schlau benennen und auf den Server kopieren.
-# 
-class ShowText(Program):
-=======
+
 # For Unicode one could use this workaround...
 # https://stackoverflow.com/questions/71962098/python-opencv-puttext-show-non-ascii-unicode-utf-character-symbols
 
 class Viedo(Program):
-<<<<<<< HEAD
-=======
->>>>>>> d179970 (fixed typos)
->>>>>>> main
+
     def __init__(self, config):
         self.config = config
-        self.config.registerKey('Video', {'default':"https://www.youtube.com/watch?v=dQw4w9WgXcQ", 'minlen':3, 'maxlen':100, 'type':str})
+        self.config.registerKey('Video', {'default':"NeverGonnaGive.avi", 'minlen':3, 'maxlen':100, 'type':str})
         self.bbox = False
         self.initVideo()
 
 
     def initVideo(self):
-        global cap = cv2.VideoCapture(self)
+        global cap
+        cap = cv2.VideoCapture(self)
         
 
     #computes the transform from LED-Coordinates (y/z) to image coordinates (w,h)
