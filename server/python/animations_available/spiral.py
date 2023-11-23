0714204bf84a75813c8b4a75bd9c2cfaaf42b798
Jonas Gassmann <jonas.g.g@icloud.com>
2023-11-23 12:09:55 +0100

    umdrehungen param

--- server/python/animations_available/spiral.py
+++ server/python/animations_available/spiral.py
@@ -9,22 +9,23 @@ class spiral(Program):
     def __init__(self, config):
         self.config = config
         self.start = time.time()
-       
+        self.config.registerKey('umdrehungen', {'default':8, 'low':1, 'high':20, 'type':float})   
+
     def step(self, leds, points):
         t = time.time()-self.start
         # Variablennamen treffender wählen, z.B. auch
         # mehr als ein Buchstabe!
         # r für Radius, x,y für Koordinaten, t für Zeit ist ok.
         # Evtl. w für Winkel, aber besser alpha.
-        Umdrehungen = 8
+        Umdrehungen = self.config['umdrehungen']
         b = [int(255*self.config['brightness']),0,0]
         d = [0,int(255*self.config['brightness']),0]
         r = 25
         beta = Umdrehungen*2*math.pi
-        alpha = 3*t % beta 
+        alpha = Umdrehungen/2*t % beta 
 
-        x = ((2-200*(alpha)/(beta))/4*math.sin(x))
-        y = ((2-200*(alpha)/(beta))/4*math.cos(x))
+        x = ((2-200*(alpha)/(beta))/4*math.sin(alpha))
+        y = ((2-200*(alpha)/(beta))/4*math.cos(alpha))
         z = ((200*(alpha)/(beta)))
         spiral = [x, y, z]
         for l in range(leds.n):
@@ -39,8 +40,8 @@ class spiral(Program):
         
 
     def defaults(self):
-        return {'params':{'brightness':0.2},
+        return {'params':{'brightness':0.2,'umdrehungen':8},
                 'autoPlay':True,
-                'playFor':20,
+                'playFor':45,
                 'web':True
                 }
