281e285bf31067822e739e6dad81e36492b82741
Constantin <knights-pennies-0t@icloud.com>
2023-11-23 12:00:18 +0100

    fexed bugs

--- server/python/animations_available/video.py
+++ server/python/animations_available/video.py
@@ -16,14 +16,13 @@ import cv2
 class Viedo(Program):
     def __init__(self, config):
         self.config = config
-        self.config.registerKey('Video', {'default':"https://www.youtube.com/watch?v=dQw4w9WgXcQ", 'minlen':3, 'maxlen':100, 'type':str})
+        self.config.registerKey('Video', {'default':"neverGonaGive.avi", 'minlen':3, 'maxlen':100, 'type':str})
         self.bbox = False
         self.initVideo()
 
 
     def initVideo(self):
-        global cap
-        cap = cv2.VideoCapture(self)
+        self.cap = cv2.VideoCapture(self)
         
 
     #computes the transform from LED-Coordinates (y/z) to image coordinates (w,h)
@@ -35,15 +34,16 @@ class Viedo(Program):
             self.dy = self.width-(self.scale*self.bbox[0]+self.offset_w)
 
     #asigns values to LEDs
-    def frame(self):
-        resize = cv2.resize(cap.read, (300, 300), interpolation = cv2.INTER_LINEAR) 
+    def step(self,leds,points):
+        resize = cv2.resize(self.cap.read, (300, 300), interpolation = cv2.INTER_LINEAR) 
         for l in range(leds.n):
             py = points[1,l]
             pz = points[2,l]
             w = int(self.scale*py+self.offset_w+dy)
             h = int(-self.scale*pz+self.offset_h)
-            v = (resize[w, h])
-            LEDs.setColor(l, v)
+            if w >=0 and h >=0 and w < 300 and h < 300:
+                v = (resize[w, h])
+                LEDs.setColor(l, v)
         
 
     def defaults(self):
