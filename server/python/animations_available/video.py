228e4d0a5b383cc80b965974b44c0cb09fd19d92
Constantin <knights-pennies-0t@icloud.com>
2023-11-28 15:27:58 +0100

    kein bock mehr

--- server/python/animations_available/video.py
+++ server/python/animations_available/video.py
@@ -28,26 +28,18 @@ class Video(Program):
         self.cap = cv2.VideoCapture("videos/"+self.config['video'])
         
 
-    #computes the transform from LED-Coordinates (y/z) to image coordinates (w,h)
-    def computeTransform(self):
-        if self.bbox:
-            self.scale = self.height/(self.bbox[3]-self.bbox[1])
-            self.offset_h = self.scale*self.bbox[3]
-            self.offset_w = -self.scale*self.bbox[2]
-            self.dy = self.width-(self.scale*self.bbox[0]+self.offset_w)
 
     #asigns values to LEDs
     def step(self,leds,points):
-        if not self.bbox:
-            self.bbox = [np.min(points[1,:]), np.min(points[2,:]), np.max(points[1,:]), np.max(points[2,:])]
-            self.computeTransform()
         ok, img = self.cap.read()
-        resize = cv2.resize(img, (300, 300), interpolation = cv2.INTER_LINEAR) 
+        resize = cv2.resize(img, (200, 200), interpolation = cv2.INTER_LINEAR) 
         for l in range(leds.n):
             py = points[1,l]
             pz = points[2,l]
-            w = int(self.scale*py+self.offset_w+self.dy)
-            h = int(-self.scale*pz+self.offset_h)
+            #w = int(self.scale*py+self.offset_w+self.dy)
+            #h = int(-self.scale*pz+self.offset_h)
+            w = py + 100
+            h = 200-pz
             print(f"w=#{w}, h=#{h}")
             if w >=0 and h >=0 and w < 300 and h < 300:
                 v = resize[w, h]
