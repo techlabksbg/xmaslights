6eec361fe55b9e21a348a299b4b755b5782f366a
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-11-28 20:20:55 +0100

    simplified, added manual mask.png

--- ledpositions/02-analyze_images.py
+++ ledpositions/02-analyze_images.py
@@ -5,9 +5,11 @@ import re
 import numpy as np
 
 n=0 #define immage counter
+mask = None
 
 # Produces an array with all files
 def init():
+    global mask
     if len(sys.argv)<2:
         print("Bitte das Verzeichnis mit den Bildern angeben.")
         print("z.B. python analyze_images.py temp/20230909_192342 > ausgabe_datei.txt")
@@ -20,7 +22,10 @@ def init():
         with open(dir+"/campos.txt") as f:
             print(f.read(), end="")
     else:
-        print("? ? ?\n? ?");
+        print("? ? ?\n? ?")
+    maskpath = dir+"/mask.png"
+    if os.path.exists(maskpath):
+        mask = cv2.imread(maskpath)
     return sorted([dir+"/"+f for f in os.listdir(dir) if re.match(r"\d{4}\.png$",f)])
 
 def find_brightest_pixel(image):
@@ -54,19 +59,16 @@ data = []  # n entries of the form [(((x,y),br),prob), ...]
 images = []
 imagesum = None
 
-polygon = np.matrix([[180,480],[180,0],[460,0], [460, 480]]).transpose()
-mask = np.zeros([480,640,3], dtype=np.uint8)
-cv2.fillPoly(mask, pts=np.int32([polygon.transpose()]),color=(255,255,255))
+if mask is None:
+    polygon = np.matrix([[180,480],[180,0],[460,0], [460, 480]]).transpose()
+    mask = np.zeros([480,640,3], dtype=np.uint8)
+    cv2.fillPoly(mask, pts=np.int32([polygon.transpose()]),color=(255,255,255))
 
-# First pass:
+data = []
 for n,f in enumerate(files):
     data.append([])
     image = cv2.imread(f)
     image = cv2.bitwise_and(image, mask)
-    images.append(image)
-    if (type(imagesum)==type(None)):
-        imagesum = np.zeros(image.shape, dtype=np.uint32)
-    imagesum += image
     for method in [simple, threshold_blurred]:
         d = method(image, data[n])
         d = (d[0][0][0], d[0][0][1], d[0][1], d[1])
@@ -74,67 +76,16 @@ for n,f in enumerate(files):
     for i,p in enumerate(data[n]):
         color = ((0,0,255), (0,255,0), (255,0,0))[i]
         cv2.circle(image, (p[0], p[1]), 8+4*i, color,  2)
-#    cv2.imshow("image", image)
-#    cv2.waitKey(50)
+    cv2.imshow("image", image)
+    cv2.waitKey(50)
 
-
-imagesum //= len(files)
-imagesum = np.uint8(imagesum)
-
-
-
-
-numleds = len(data)
 bottom_n = 0 # max(range(numleds), key=lambda i:data[i][2][0][0][1])
-top_n = 21
 top_n = min([i for i in range(numleds) if abs(data[i][1][0]-data[bottom_n][1][0])<20], key=lambda i:data[i][1][1])
-
-#print("top_n=%d, bottom_n=%d (should be 0)" % (top_n, bottom_n))
-#print(data[top_n])
-#print(data[bottom_n])
-
-top = data[top_n][1][0:3]
-bottom = data[bottom_n][1][0:3]
-
-e2 = np.array([top[0]-bottom[0],top[1]-bottom[1]])
-e1 = np.array([-e2[1], e2[0]])
-o = np.array([bottom[0], bottom[1]])
-
-a = np.matrix([e1, e2, o]).transpose()
-#print(a);
-
-polygon = np.matrix([[0,-0.05,1],[0.4,0.05,1],[0.05, 1.05,1], [-0.05, 1.05 ,1], [-0.4,0.05,1]]).transpose()
-
-points = a*polygon;
-#print(points);
-
-mask = np.zeros(image.shape, dtype=np.uint8)
-cv2.fillPoly(mask, pts=np.int32([points.transpose()]),color=(255,255,255))
-
-#cv2.imshow("mask", mask)
-#cv2.waitKey(10)
-
-# Second pass:
-data1st = data
-data = []
-for n,img in enumerate(images):
-    data.append([])
-    image = images[n]#-cv2.min(images[n],imagesum)
-    image = cv2.bitwise_and(image, mask)
-    for method in [simple, threshold_blurred]:
-        d = method(image, data[n])
-        d = (d[0][0][0], d[0][0][1], d[0][1], d[1])
-        data[n].append(d)
-    for i,p in enumerate(data[n]):
-        color = ((0,0,255), (0,255,0), (255,0,0))[i]
-        cv2.circle(image, (p[0], p[1]), 8+4*i, color,  2)
-#    cv2.imshow("image", image)
-#    cv2.waitKey(50)
 
 data2 = data
 data = []
 distances=[]
-for n in range(len(images)):
+for n in range(numleds):
     p1 = np.array(data2[n][0][0:2])
     p2 = np.array(data2[n][1][0:2])
     if np.linalg.norm(p1-p2)<5:
