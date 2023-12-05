c888ab0696e294fbe726fedc3379a97da348a3c3
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-12-05 09:02:40 +0100

    newest Ebene.py

--- server/python/animations_available/Ebene.py
+++ server/python/animations_available/Ebene.py
@@ -4,48 +4,36 @@ import colorsys
 import numpy as np
 import math
 
+
+
 class Ebene(Program):
-    def __init__(self, config):
+    def __init__(self, config): # wird einmal aufgerufen, danach nie mehr, wird festgelegt, was ich am Programm benötige
         self.config = config
         self.start = time.time()
-        self.config.registerKey('dir', {'array':True, 'len':3, 'low':-1, 'high':1, 'default':[0,0,1], 'type':float})
-        self.config.registerKey('scale', {'default':100, 'low':5, 'high':500, 'type':float})
 
-    def step(self, leds, points):
+    def step(self, leds, points): # wird immer wieder aufgerufen, wie der Baum aussieht
         dt = (time.time()-self.start)/self.config['period']
         
-        hues = np.matmul(np.array([math.cos(dt), math.sin(dt), 0]), points)/self.config['scale']+(1-dt%1)
+        hues = np.matmul(np.array([math.cos(dt), math.sin(dt), 0]), points) # Farbenberechnung, Entfernung aller Punke der drehenden Ebene
         for l in range(leds.n):
-            h = hues[l] % 1
-            c : list(int,int,int) = [int(x*255) for x in colorsys.hsv_to_rgb(h, self.config['saturation'], self.config['brightness'])]
-            leds.setColor(l, c)
-
+            x = - 60
+            i = 0       # Überwachung der Anzahl schleifendurchläufe
+            while x <60:
+                i = i + 1
+                if hues[l] < x: # Setzen der Farbe an jedem zweiten Durchlauf
+                    if i%2 == 0:
+                        c = [int(255*self.config["brightness"]),0,0]
+                    else:
+                        c = [0,int(255*self.config["brightness"]),0]
+                    break
+                x = x + 120/8
+            leds.setColor(l, c) # Festlegung der Farbe
+        
     def defaults(self):
-        return {'params':{'brightness':0.1, 
-                          'saturation':1.0, 
-                          'scale':100},
+        return {'params':{'brightness':0.2,
+                          'period':3,
+                          'color':"000000"},
                 'autoPlay':True,
-                'playFor':20,
+                'playFor': 3000, # Wie lange sollte mein Programm laufen, bevor es wechselt (in Sekunden)?
                 'web':True}
 
-    # Wozu sind die Variablen position und farbe?
-    def LED(position,farbe):
-        by=120
-        # Achtung, erst e1,e2,e3,e4 definieren, dann etwas damit machen
-        # nicht umgekehrt.
-        ey=[e1,e2,e3,e4]
-        e1=0
-        e2=by*1/4
-        e3=by*2/4
-        e4=by*3/4
-        # Was ist [1][l] ??
-        for l in range(l):
-            if [1][l] < e4:
-                if [1][l] < e3:
-                    if [1][l] < e2:
-                        if [1][l] < e1:
-                            c = [255,255,0]
-                        else: c = [255,255,255]
-                    else: c = [255,255,0]
-                else: c = [255,255,255]
-        
