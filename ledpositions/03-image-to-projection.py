e977d7c30331edd7cd7f25dd25bb853da5d66867
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-12-19 08:06:11 +0100

    still buggy

--- ledpositions/03-image-to-projection.py
+++ ledpositions/03-image-to-projection.py
@@ -40,20 +40,24 @@ spq = (data['leds'][bottom_n]['point'],
 
 # Camera position
 kxyz = data['cam']
-# Vector b
+# Vector b in image coordinates
 b = spq[1]-spq[0]
 print(f"kxyz = {kxyz}")
 camtree = np.linalg.norm(kxyz[0:2])  # Distance cam to tree (in cm)
 print(f"camtree = {camtree}")
-middleToTree = abs(data['leds'][bottom_n]['point'][0]-320)/np.linalg.norm(b)*data['height']  # Distance to image center in cm
+middleToTree = (data['leds'][bottom_n]['point'][0]-320)/np.linalg.norm(b)*data['height']  # Distance to image center in cm
 print(f"bottom = {data['leds'][bottom_n]['point']}, middleToTree={middleToTree}")
 hfp = middleToTree*middleToTree/camtree  # Distance to height onto camtree
-h = (hfp*(middleToTree-hfp))**0.5   # Length of height to from camtree to image center
+h = (hfp*(camtree-hfp))**0.5   # Length of height to from camtree to image center
 print(f"hfp = {hfp}, h={h}")
 k = kxyz[0:2]
 k /= np.linalg.norm(k)
 kperp = np.array([-k[1], k[0]])
-planex= -k*hfp+h*kperp
+print(f"k={k}, kperp={kperp}")
+if middleToTree>0:
+    planex= -(k*hfp-h*kperp)
+else:
+    planex= k*hfp+h*kperp
 planex *= data['height']/np.linalg.norm(planex)
 
 # Rotation about +90 degrees (from the image's x- to the  y-axis)
@@ -72,8 +76,9 @@ points = np.append(points, [np.ones(numleds)], axis=0)
 u = np.array([-kxyz[1], kxyz[0], 0]) #+90 degree rotation of cam vector
 u = u/np.linalg.norm(u)*data['height']  #adjust length
 print(f"old u={u}")
-print(f"planex = {planex}")
-#u = [planex[0], planex[1], 0]
+#print(f"planex = {planex}")
+u = np.array([planex[0], planex[1], 0])
+print(f"new u = {u}")
 
 v = np.array([0,0,data['height']])  # straight up
 
@@ -86,13 +91,16 @@ print(f"a={a},\n b={b}")
 #   (Alternatively one could use the '@'-operator to multiply matrices)
 t = np.matmul(b, np.linalg.inv(a))
 
+print(f"t = {t}")
+
 # Projected points
 projected = np.matmul(t, points)
 
+
 # Output
 with open(data['output'], "w") as f:
-    print("%.1f %.1f %.1f" % (kxyz[0], kxyz[1], kxyz[2]))
+    #print("%.1f %.1f %.1f" % (kxyz[0], kxyz[1], kxyz[2]))
     f.write("%.1f %.1f %.1f\n" % (kxyz[0], kxyz[1], kxyz[2]))
     for i in range(numleds):
-        print("%.1f %.1f %.1f %.3f" % (projected[0][i], projected[1][i], projected[2][i], data['leds'][i]['confidence']))
+        #print("%.1f %.1f %.1f %.3f" % (projected[0][i], projected[1][i], projected[2][i], data['leds'][i]['confidence']))
         f.write("%.1f %.1f %.1f %.3f\n" % (projected[0][i], projected[1][i], projected[2][i], data['leds'][i]['confidence']))
